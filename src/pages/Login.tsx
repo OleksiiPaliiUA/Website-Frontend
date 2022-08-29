447b7e14817d5668d9588ba19b432760e9b86e1d
Oleksii Palii <alexey.paliy1337@gmail.com>
2022-08-29 21:27:52 +0300

    Rebuild Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import axios from 'axios';
+import { SyntheticEvent, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import '../Login.css'
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [redirect, setRedirect] = useState(false)
+    const [toSignUpPage, setToSignUpPage] = useState(false)
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault()
+
+        const {status} = await axios.post('http://localhost:8000/api/login', {
+            email,
+            password
+        }, {withCredentials: true})
+
+        if(status === 201){
+            setRedirect(true)
+        }
+    }
+
+    if(toSignUpPage) {
+        return <Navigate to={'/register'} />
+    }
+
+    if(redirect) {
+        return <Navigate to={'/'} />
+    }
+    
     return (
-        <div>
-            Login Page
+        <div className='page-signin'>
+            <main className="form-signin w-100 m-auto">
+                <form onSubmit={submit} className='components-signin'>
+                    <div className='text-center'>
+                        <img src="logo192.png" alt='' />
+                        <h3 className="h2 mb-3 fw-normal">Welcome</h3>
+                        <h5 className="h5 mb-3 fw-normal">Please sign in</h5>
+                    </div>
+                    <div className="form-floating">
+                        <input type="email" className="form-control" placeholder="name@example.com" required 
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                        <label>Email address</label>
+                    </div>
+                    <div className="form-floating">
+                        <input type="password" className="form-control" placeholder="Password" id='LoginPassword' required 
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                        <label>Password</label>
+                    </div>
+                    <button type='button' className="btn" onClick={e => setToSignUpPage(true)}>Create account</button>
+                    <button type="submit" className="btn btn-primary">Sign in</button>
+                </form>
+            </main>
         </div>
     );
 };
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,84 +1,83 @@
-import {Component, SyntheticEvent} from 'react'
+import {SyntheticEvent, useState} from 'react'
 import axios from 'axios'
 import '../Login.css'
 import { Navigate } from 'react-router-dom'
 
 
-export default class Register extends Component {
+const Register = () => {
 
-    firstName = ''
-    lastName = ''
-    email = ''
-    password = ''
-    passwordConfirm = ''
-    state = {
-        redirect: false
-    }
+    const [first_name, setFirstName] = useState('')
+    const [last_name, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [password_confirm, setPasswordConfirm] = useState('')
+    const [redirect, setRedirect] = useState(false)
 
-    submit = (e: SyntheticEvent) => {
+    const submit = async (e: SyntheticEvent) => {
         e.preventDefault()
         
-        axios.post('http://localhost:8000/api/register', {
-            first_name: this.firstName,
-            last_name: this.lastName,
-            email: this.email,
-            password: this.password,
-            password_confirm: this.passwordConfirm
-        }).then(response => {
-            if(response.status === 201) {
-                this.setState({
-                    redirect: true
-                })
-            }
+        const {status} = await axios.post('http://localhost:8000/api/register', {
+            first_name,
+            last_name,
+            email,
+            password,
+            password_confirm
         })
+
+        if(status === 201) {
+            setRedirect(true)
+        }
     }
 
-    render() {
-
-        if(this.state.redirect) {
-            return <Navigate to={'/login'}/>
-        }
-        
-        return (
-            <div>
-                <main className="form-signin w-100 m-auto">
-                    <form onSubmit={this.submit}>
-                        <h1 className="h3 mb-3 fw-normal">Please sign Up</h1>
-
-                        <div className="form-floating">
+    if(redirect) {
+        return <Navigate to={'/login'}/>
+    }
+    
+    return (
+        <div className="page-signin">
+            <main className="form-signin w-100 m-auto">
+                <form onSubmit={submit} className='components-signin'>
+                    <div className='text-center'>
+                        <img src="logo192.png" alt='' />
+                        <h3 className="h2 mb-3 fw-normal">Welcome</h3>
+                        <h5 className="h5 mb-3 fw-normal">Please sign up</h5>
+                    </div>
+                    <div className="form-floating">
                         <input type="textFirst" className="form-control" placeholder="Oleksii" required
-                            onChange={e => this.firstName = e.target.value}
+                            onChange={e => setFirstName(e.target.value)}
                         />
                         <label>First name</label>
-                        </div>
-                        <div className="form-floating">
+                    </div>
+                    <div className="form-floating">
                         <input type="textLast" className="form-control" placeholder="Palii" required
-                            onChange={e => this.lastName = e.target.value}
+                            onChange={e => setLastName(e.target.value)}
                         />
                         <label>Last name</label>
-                        </div>
-                        <div className="form-floating">
+                    </div>
+                    <div className="form-floating">
                         <input type="email" className="form-control" placeholder="name@example.com" required
-                            onChange={e => this.email = e.target.value}
+                            onChange={e => setEmail(e.target.value)}
                         />
                         <label>Email address</label>
-                        </div>
-                        <div className="form-floating">
+                    </div>
+                    <div className="form-floating">
                         <input type="password" className="form-control" placeholder="Password" required
-                            onChange={e => this.password = e.target.value}
+                            onChange={e => setPassword(e.target.value)}
                         />
                         <label>Password</label>
-                        </div>
-                        <div className="form-floating">
+                    </div>
+                    <div className="form-floating">
                         <input type="password" className="form-control" placeholder="Password Confirm" required
-                            onChange={e => this.passwordConfirm = e.target.value}
+                            onChange={e => setPasswordConfirm(e.target.value)}
                         />
                         <label>Password Confirm</label>
-                        </div>
-                        <button className="w-100 btn btn-lg btn-primary" type="submit">Sign Up</button>
-                    </form>
-                </main>
-            </div>
-        )
-    }
+                    </div>
+                    <button className="btn" type="button" onClick={e => setRedirect(true)}>Sign in</button>
+                    <button className="btn btn-primary" type="submit">Sign up</button>
+                </form>
+            </main>
+        </div>
+    )
 }
+
+export default Register;
